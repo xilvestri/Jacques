d7ded11686ad3e9a29374e0c0f09097cb728bcb8
xilvestri <stark324@gmail.com>
2015-03-14 17:44:58 -0400

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -63,27 +63,35 @@ def calibration(state):
                 Flame1reading = ADC.read_raw(Flame1)
                 Flame2reading = ADC.read_raw(Flame2)
                 Flame3reading = ADC.read_raw(Flame3)
+                
                 noFlame1 = noFlame1 + [Flame1reading]
                 noFlame2 = noFlame2 + [Flame2reading]
                 noFlame3 = noFlame3 + [Flame3reading]
+
             
             #eliminate outliers if found
-            noFlame1=[e for e in noFlame1 if (np.mean(noFlame1)-2*np.std(noFlame1) < e <np.mean(noFlame1) +2 * np.std(noFlame1))]
-            noFlame2=[e for e in noFlame2 if (np.mean(noFlame2)-2*np.std(noFlame2) < e <np.mean(noFlame2) +2 * np.std(noFlame2))]
-            noFlame3=[e for e in noFlame3 if (np.mean(noFlame3)-2*np.std(noFlame3) < e <np.mean(noFlame3) +2 * np.std(noFlame3))]
+            noFlame1=[e for e in noFlame1 if (np.median(noFlame1)-2*np.std(noFlame1) < e <np.median(noFlame1) +2 * np.std(noFlame1))]
+            noFlame2=[e for e in noFlame2 if (np.median(noFlame2)-2*np.std(noFlame2) < e <np.median(noFlame2) +2 * np.std(noFlame2))]
+            noFlame3=[e for e in noFlame3 if (np.median(noFlame3)-2*np.std(noFlame3) < e <np.median(noFlame3) +2 * np.std(noFlame3))]
             
             #takes average of values and stores for future use.
             noAverage1 = np.mean(noFlame1)
+            if noAverage1== "nan":
+                yesAverage1=1799.0
             noAverage2 = np.mean(noFlame2)
+            if noAverage2== "nan":
+                yesAverage2=1799.0
             noAverage3 = np.mean(noFlame3)
+            if noAverage3== "nan":
+                yesAverage3=1799.0
             
             #reads a thermistor value
             nothermistor=ADC.read_raw(thermistor)
 
-            #print "no"
-            #print noAverage1
-            #print noAverage2
-            #print noAverage3
+            print "no"
+            print noAverage1
+            print noAverage2
+            print noAverage3
 
             #first collection complete. Flag for second collection
             Cflag=1
@@ -104,30 +112,43 @@ def calibration(state):
                 yesFlame2 = yesFlame2 + [Flame2reading]
                 yesFlame3 = yesFlame3 + [Flame3reading]
 
-            yesFlame1=[e for e in yesFlame1 if (np.mean(yesFlame1)-2*np.std(yesFlame1) < e <np.mean(yesFlame1) +2 * np.std(yesFlame1))]
-            yesFlame2=[e for e in yesFlame2 if (np.mean(yesFlame2)-2*np.std(yesFlame2) < e <np.mean(yesFlame2) +2 * np.std(yesFlame2))]
-            yesFlame3=[e for e in yesFlame3 if (np.mean(yesFlame3)-2*np.std(yesFlame3) < e <np.mean(yesFlame3) +2 * np.std(yesFlame3))]
+            yesFlame1=[e for e in yesFlame1 if (np.median(yesFlame1)-2*np.std(yesFlame1) < e <np.median(yesFlame1) +2 * np.std(yesFlame1))]
+            yesFlame2=[e for e in yesFlame2 if (np.median(yesFlame2)-2*np.std(yesFlame2) < e <np.median(yesFlame2) +2 * np.std(yesFlame2))]
+            yesFlame3=[e for e in yesFlame3 if (np.median(yesFlame3)-2*np.std(yesFlame3) < e <np.median(yesFlame3) +2 * np.std(yesFlame3))]
             
             yesAverage1 = np.mean(yesFlame1)
+            if yesAverage1== "nan":
+                yesAverage1=1799.0
             yesAverage2 = np.mean(yesFlame2)
+            if yesAverage2== "nan":
+                yesAverage2=1799.0
             yesAverage3 = np.mean(yesFlame3)
+            if yesAverage3== "nan":
+                yesAverage3=1799.0
+            
+            lowestAverage=min(yesAverage1,yesAverage2,yesAverage3)
             
         
             yesthermistor=ADC.read_raw(thermistor)
-            #print "yes"
-            #print yesAverage1
-            #print yesAverage2
-            #print yesAverage3
+            print "yes"
+            print yesAverage1
+            print yesAverage2
+            print yesAverage3
             
-            state=4          #Program goes directly to search phase
+            
             
             #Check for validity of values. Checks for large gap between no flame and at flame readings. 
             #Also prompts for redo when too many outliers found
-            #if noAverage1 > yesAverage1 *2 and noAverage2 > yesAverage2 and noAverage3 > yesAverage3 and len(yesFlame1)>40 and len(yesFlame2)>40 and len(yesFlame3)>40 and len(noFlame1)>40 and len(noFlame2)>40 and len(noFlame3)>40:
-                #Cflag = 0 
-                #state = 2
+            if noAverage1 < lowestAverage *2 or noAverage2 < lowestAverage *2 or noAverage3 < lowestAverage*2 or len(yesFlame1)<40 or len(yesFlame2)<40 or len(yesFlame3)<40 or len(noFlame1)<40 or len(noFlame2)<40 or len(noFlame3)<40:
+                Cflag = 0 
+                state = 2
+                print"problem"
+ 
             
-            Cflag=2
+            else:
+                state=4 #Program goes directly to search phase
+                print"ready to search!"
+                Cflag=2
 
             
-    return {'state':state, 'maxVal1': noAverage1, 'maxVal2':noAverage2,'maxVal3': noAverage3,'minVal1': yesAverage1,'minVal2': yesAverage2,'minVal3': yesAverage3, 'maxTherm':yesthermistor, 'minTherm': nothermistor}
+    return {'state':state, 'maxVal1': noAverage1, 'maxVal2':noAverage2,'maxVal3': noAverage3,'minVal': lowestAverage, 'maxTherm':yesthermistor, 'minTherm': nothermistor}
