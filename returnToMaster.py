525085d47dd264f1788d15b1490dbe638a2f2e94
xilvestri <stark324@gmail.com>
2015-03-23 00:40:22 -0400

    Update and rename return.py to returnToMaster.py
    Built to include rear sonar. Currently backs up until something is behind it.

--- returnToMaster.py
+++ returnToMaster.py
@@ -3,16 +3,42 @@ import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
 import time
 import search
+import Adafruit_BBIO.UART as UART
+import serial
 
+Ultra1E="P9_15"
+Ultra1T="P9_13"
 Status5="P8_17"
 SButton= "P8_10"
+
+Status1="P8_11" 
+Status2="P8_12"
+Status3="P8_13"
+Status4="P8_14"
+Status5="P8_17"
+
+GPIO.setup(Ultra1E, GPIO.IN)
+GPIO.setup(Ultra1T, GPIO.OUT)
+
+GPIO.setup(Status1, GPIO.OUT)
+GPIO.setup(Status2, GPIO.OUT)
+GPIO.setup(Status3, GPIO.OUT)
+GPIO.setup(Status4, GPIO.OUT)
+GPIO.setup(Status5, GPIO.OUT)
+GPIO.setup(SButton, GPIO.IN)
+
 ADC.setup()
 
 blinkCount=0
 
+UART.setup("UART1")
+ser = serial.Serial(port = "/dev/ttyO1", baudrate = 9600)
+
 def returnToMaster(state):
     here=0                           #not back to master now
     blinkCount=0
+    driveCom="F"
+    ser.write(driveCom)
     
     while state == 10:
         if(blinkCount<=700):
@@ -27,12 +53,13 @@ def returnToMaster(state):
         if (GPIO.input(SButton) == 1):
             state=13        #brings to pause state for manual advance
         
-   
-        #Put return code here
-        driveCom="F"
-        ser.write(driveCom)
+  
+        if driveCom != "F":
+            driveCom="F"
+            ser.write(driveCom)
+            
         SonarR = search.readSonar(Ultra1T, Ultra1E)
-        if SonarR<=20:
+        if SonarR<=30:
             driveCom="G"
             ser.write(driveCom)
             GPIO.output(Status1, GPIO.HIGH)
@@ -64,12 +91,42 @@ def returnToMaster(state):
             GPIO.output(Status3, GPIO.HIGH)
             GPIO.output(Status4, GPIO.LOW)
             GPIO.output(Status5, GPIO.HIGH)
+            time.sleep(1.1)
+            GPIO.output(Status1, GPIO.LOW)
+            GPIO.output(Status2, GPIO.HIGH)            
+            GPIO.output(Status3, GPIO.LOW)
+            GPIO.output(Status4, GPIO.HIGH)
+            GPIO.output(Status5, GPIO.LOW) 
+            time.sleep(1.1)
+            GPIO.output(Status1, GPIO.HIGH)
+            GPIO.output(Status2, GPIO.LOW)            
+            GPIO.output(Status3, GPIO.HIGH)
+            GPIO.output(Status4, GPIO.LOW)
+            GPIO.output(Status5, GPIO.HIGH)
+            time.sleep(1.1)
+            GPIO.output(Status1, GPIO.LOW)
+            GPIO.output(Status2, GPIO.HIGH)            
+            GPIO.output(Status3, GPIO.LOW)
+            GPIO.output(Status4, GPIO.HIGH)
+            GPIO.output(Status5, GPIO.LOW) 
+            time.sleep(1.1)
+            GPIO.output(Status1, GPIO.HIGH)
+            GPIO.output(Status2, GPIO.LOW)            
+            GPIO.output(Status3, GPIO.HIGH)
+            GPIO.output(Status4, GPIO.LOW)
+            GPIO.output(Status5, GPIO.HIGH)
+            time.sleep(1.1)
+            GPIO.output(Status1, GPIO.LOW)
+            GPIO.output(Status2, GPIO.LOW)            
+            GPIO.output(Status3, GPIO.LOW)
+            GPIO.output(Status4, GPIO.LOW)
+            GPIO.output(Status5, GPIO.LOW)
             
             here=1
         
         
         if (here== 1):
-            state = 12     #allows for immediate state change to return state
+            state = 3     #allows for immediate state change to search state
             
     
     return state
