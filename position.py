77ff9d21b6e3c9e31247ddfacf705047793023c5
xilvestri <stark324@gmail.com>
2015-04-02 02:15:28 -0400

    Update position.py

--- position.py
+++ position.py
@@ -2,60 +2,71 @@
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
 import time
-import Adafruit_BBIO.UART as UART
 import serial
 
 
 Flame1 = "P9_40"
 Flame2 = "P9_38"
 Flame3 = "P9_36"
-Status4="P8_14"
-RButton= "P8_18"
+thermistor = "P9_39"
+Status3="P8_13"
 SButton= "P8_10"
-probe = 'P9_37'
 ADC.setup()
 
-UART.setup("UART1")
 ser = serial.Serial(port = "/dev/ttyO1", baudrate = 9600)
 
-def roasting(state):
+ser.open()
+
+
+def position(state, Status3,  minTherm):
     blinkCount=0
-    timeStart=time.time()
-    while state == 8:
-        if(blinkCount<=700):
-             GPIO.output(Status4, GPIO.HIGH)
-             blinkCount = blinkCount+1
-        elif(700<blinkCount<=1400):
-             GPIO.output(Status4, GPIO.LOW)
-             blinkCount = blinkCount +1
+    startCount =0
+    servoCount =0
+    thermistor="P9_39"
+    
+    # spinMallow=0                   #spin the mallow!
+    # ser.write(spinMallow)
+    time.sleep(.2)
+    servoCom="Y"                #set servo to start position
+    ser.write(servoCom) 
+    
+    while state == 6:
+        if (GPIO.input(SButton) == 1):
+            state = 7                                #return a state of 7 so manual state control can occur
+            
+        if(blinkCount<=1):
+            GPIO.output(Status3, GPIO.HIGH)
+            blinkCount = blinkCount+1
+        elif(1<blinkCount<=3):
+            GPIO.output(Status3, GPIO.LOW)
+            blinkCount = blinkCount +1
         else:
             blinkCount=0
             
-        reading = ADC.read_raw(probe)
         
-        if (reading> 1444):
-            # Position servo in up position
-            state = 10     #allows for immediate state change to return state
+        servoCom="W"                    #move servo down (6) degrees 
+        ser.write(servoCom)
+        
+        
+        #reads thermistor to check quality of position
+        time.sleep(.7)
+        thermistor=ADC.read_raw("P9_39")
+        
+        servoCount = servoCount +1
+        
+        #if thermistor angle is where we want it
+        if(thermistor>(50+minTherm)):
+            state = 8    #allows for immediate state change to roasting
+         
+        
+        elif (servoCount >= 33):
+            servoCount =0
             servoCom="Y"                #set servo to start position
-            ser.write(servoCom)
-            GPIO.output(Status4, GPIO.LOW)
-            # pinMallow="b"                   #stop the mallow!
-            # ser.write(spinMallow)
-            
-        if (GPIO.input(SButton) == 1):
-            state=9        #brings to pause state for manual advance
-        
-        timeNow=time.time()
-        timePass=timeNow-timeStart
-        if timeStart > 120 and reading<1370:
-            while(state==8):
-                GPIO.output(Status1, GPIO.HIGH)
-                GPIO.output(Status2, GPIO.LOW)            #LED pattern for failed roast
-                GPIO.output(Status3, GPIO.HIGH)
-                GPIO.output(Status4, GPIO.LOW)
-                GPIO.output(Status5, GPIO.HIGH)
-                if (GPIO.input(SButton) == 1):
-                    state=7        #brings to pause state for manual advance
+            ser.write(servoCom)     
+            startCount=startCount +1    #try again?
+            if startCount==2:           #go back to search
+                GPIO.output(Status3, GPIO.LOW)
+                state=4
             
     
     return state
